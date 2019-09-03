cdaa169494e2aa8976a39e5e587914d3c71846e9
Filippo <filippo.roggia@cambieri.it>
2019-09-03 16:36:55 +0200

    none

--- LetturaFile/letturaPDF.js
+++ LetturaFile/letturaPDF.js
@@ -1,7 +1,11 @@
 const fs=require('fs');
 //var ext=process.argv[2];
 
+<<<<<<< Updated upstream
 fs.readFile('./idroterm.pdf',  function(error, data){
+=======
+fs.readFile('C:/Users/filip/Desktop/NPM prova/Pdf/deltaterm.pdf',  function(error, data){
+>>>>>>> Stashed changes
    
     if (error) throw err;
 
